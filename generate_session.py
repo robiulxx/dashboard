06da8f713d1c0418a13fb861d4bd9496ef42cf87
robiulxx <86838778+robiulxx@users.noreply.github.com>
2025-09-25 04:43:00 +0600

    Update generate_session.py

--- generate_session.py
+++ generate_session.py
@@ -2,7 +2,7 @@ from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
 
-API_ID = ("24595217"))
+API_ID = ("24595217")
 API_HASH = ("8b37bc475b594f5f30e6b2c01699366e")
 BOT_TOKEN = ("8249949820:AAFzjIiJJx5cARdEQV4jz6nmb2KpiSmSrVw")
 
