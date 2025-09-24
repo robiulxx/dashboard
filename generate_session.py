c2086ca13fb2063e06e1e95b82b0a9d7129bcef7
robiulxx <86838778+robiulxx@users.noreply.github.com>
2025-09-25 04:37:51 +0600

    Update generate_session.py

--- generate_session.py
+++ generate_session.py
@@ -2,9 +2,9 @@ from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("24595217"))
+API_HASH = os.getenv("8b37bc475b594f5f30e6b2c01699366e")
+BOT_TOKEN = os.getenv("8249949820:AAFzjIiJJx5cARdEQV4jz6nmb2KpiSmSrVw")
 
 with TelegramClient(StringSession(), API_ID, API_HASH) as client:
     client.start(bot_token=BOT_TOKEN)
